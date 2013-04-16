ed92c0369217c024ccb76637948a8c629a7951df
Matheus <matheus@matheus-notebook.(none)>
2013-04-16 03:50:11 -0300

    Add place to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,9 @@ class Event < ActiveRecord::Base
   belongs_to :calendar
   has_many :event_schedules
 
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :event_schedules_attributes
+  accepts_nested_attributes_for :event_schedules
 
   validates :title, :presence => true
+  validates_associated :event_schedules
 end
